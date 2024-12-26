df25862e6f2d2f91d34aa4ca198c07e341be1f23
Marcus Amaral <127886169+marcuslucamaral@users.noreply.github.com>
2024-12-26 13:24:24 -0300

    Update projetil_list1_q9_t2.f90

--- lista_1/projetil_list1_q9_t2.f90
+++ lista_1/projetil_list1_q9_t2.f90
@@ -1,10 +1,46 @@
-!===========================================
-!===========================================
-!Programador: Marcus Lucas Amaral do Amaral
-!Número de matrícula:202301670006 
-!Data:13/04/2023
-!===========================================
-!================
+!================================================================================
+ 
+    ! Purpose:
+    !
+    ! Um dispositivo para lançar projéteis (um canhão, ou morteiro) é capaz de lançar uma
+    !bala com velocidade de módulo v0 em um ângulo de lançamento teta. Considere que um
+    !lançamento é feito do alto de um penhasco de altura h, como ilustrado na figura.
+    !Se ignorarmos a influência do ar e as variações na aceleração da gravidade g com a altura
+    !(uma aproximação ruim para velocidades altas) o projétil irá viajar em uma trajetória
+    !parabólica e atingir o solo a uma distância horizontal x após um tempo t.
+    !Como você pode deduzir facilmente, o tempo para o impacto com o solo é dado pela solução
+    !da equação quadrática
+    !gt^2 - 2v0 sin(teta)t − 2h0 = 0.
+    !Uma vez tendo o tempo, a distância percorrida na horizontal será simplesmente
+    !x = v0 cos(teta)t.
+    !Escreva um programa que tenha como entrada os dados do modelo e calcule o tempo t e a
+    !posição x para o ponto de impacto com o solo, com as seguintes informações:
+    !Entradas:                                             Saídas:
+    !– Altura h;                                         – Tempo t para o impacto;
+    !– Velocidade de lançamento v0;                      – Distância x até o ponto de impacto.
+    !– Ângulo de lançamento teta.
+    !O programa pode receber os valores diretamente como entradas no teclado e dar a saída
+    !no terminal. Escreva uma subroutine ou function para calcular a solução da equação.
+    !Você pode usar a rotina que já tem para isto, ou programar uma para este caso particular.
+    !Qual você considera a melhor abordagem?
+    !
+    !Especifique que o ângulo de lançamento é em graus e que não pode ser maior do que 90.
+    !O programa deve verificar o valor do ângulo dado como entrada e pedir uma correção caso
+    !ele seja maior do que 90◦. Sua rotina irá funcionar para ângulos menores que zero? Se sim,
+    !também haverá alguma restrição de ângulo para este caso? Após se certificar das respostas
+    !a estas duas perguntas, programe as verificações sobre todas as restrições necessárias e
+    !também as inclua em linhas de comentários no seu arquivo do código.
+    !
+    !Defina a aceleração da gravidade como constante (parameter), com o valor g = 9.8 m/s2.
+    !Todos as grandezas físicas devem ser expressas em unidades do Sistema Internacional. Não
+    !esqueça de que as funções trigonométricas sin e cos esperam argumentos em radianos.
+    !
+    ! Record of revisions:
+    ! Date_begin/end_date                 Programmer                             Description of change
+    ! ========                     =============================                      =============
+    ! unknown                            Marcus Amaral                               Original code
+    ! unknown
+!================================================================================
 program trajetoria
     implicit none
     real, parameter :: g = 9.8      !Valor da Gravidade 
