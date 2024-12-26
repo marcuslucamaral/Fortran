727901b9e3f900180fdb5d6eda8025d0968ad500
Marcus Amaral <127886169+marcuslucamaral@users.noreply.github.com>
2024-12-26 01:27:09 -0300

    Update trio_pitag_lista_1_q_7.f90

--- lista_1/trio_pitag_lista_1_q_7.f90
+++ lista_1/trio_pitag_lista_1_q_7.f90
@@ -1,12 +1,22 @@
-!===========================================
-!===========================================
-!Programador: Marcus Lucas Amaral do Amaral
-!Número de matrícula:202301670006 
-!Data:13/04/2023
-!===========================================
-!===========================================
-!
-! funçao verificaçao se é triangulo valido
+!================================================================================
+ 
+    ! Purpose:
+    !
+    ! Qualquer conjunto de três números inteiros que são lados de um triângulo retângulo é
+    !chamado de Trio Pitagórico. Encontrar todos os trios pitagóricos formados por inteiros não
+    !maiores do que 500. Use laços de DO para testar todas as possibilidades. Construa os
+    !laços de maneira a não gerar triângulos repetidos. Este é um exemplo de computação de
+    !“força bruta”. Em cursos mais avançados você vai aprender que existem vários problemas
+    !interessantes para os quais não há nenhuma abordagem algorítmica conhecida a não ser
+    !usar pura força bruta.
+    !
+    !
+    ! Record of revisions:
+    ! Date_begin/end_date                 Programmer                             Description of change
+    ! ========                     =============================                      =============
+    ! 14/04/23                            Marcus Amaral                               Original code
+    ! unknown
+!================================================================================
 Program TrioPitagorico 
     Implicit None
     
